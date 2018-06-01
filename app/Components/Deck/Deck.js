b2d02884c1e20fe4f8865c9a0f3198c2c4990e62
Pedro Silva <pedro.rls93@gmail.com>
2018-06-01 19:21:49 -0300

    dumb data for test

--- app/Components/Deck/Deck.js
+++ app/Components/Deck/Deck.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text, Animated } from 'react-native';
 import styles from './style';
+import DATA from '../../config/data';
+
+console.log(DATA);
 
 export default class Deck extends Component {
     render(){
