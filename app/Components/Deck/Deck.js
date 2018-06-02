593945ab22e8f8980bfa85f0d1ed1781fc00c321
Pedro Silva <pedro.rls93@gmail.com>
2018-06-02 00:46:53 -0300

    after swipe is completed, the deck moves to the next card and shows a message if there are none

--- app/Components/Deck/Deck.js
+++ app/Components/Deck/Deck.js
@@ -7,13 +7,22 @@ import {
 } from 'react-native';
 import styles from './style';
 
+
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.5 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 200;
 
 export default class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => console.log('Like'),
+        onSwipeLeft: () => console.log('Dislike'),
+        renderNoMoreCards: () => console.log('No More Cards!'),
+    }
+
     constructor(props){
         super(props);
         const position = new Animated.ValueXY();
+
         const panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, gesture) => {
@@ -21,20 +30,36 @@ export default class Deck extends Component {
             },
             onPanResponderRelease: (event, gesture) => {
                 if(gesture.dx > SWIPE_THRESHOLD){
-                    console.log('Like!');
+                    this.swipeCard('right');
                 }else if(gesture.dx < -SWIPE_THRESHOLD){
-                    console.log('Dislike!');
+                    this.swipeCard('left');
+                }else{
+                    this.resetPosition();
                 }
-                this.resetPostion();
             }
         });
-        this.state = { panResponder, position };
+        this.state = { panResponder, position, index: 0 };
     }
 
-    resetPostion(){
-        Animated.spring(this.state.position, {
-            toValue: {x: 0, y: 0}
-        }).start();
+    renderCards(){
+        if(this.state.index >= this.props.data.length){
+            return this.props.renderNoMoreCards();
+        }
+        return this.props.data.map((item, idx)=> {
+            if(idx < this.state.index){ return null }
+            if(idx === this.state.index){
+                return(
+                    <Animated.View
+                        key={ item.id }
+                        style={ [styles.container, this.cardAnimationStyle()] }
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        { this.props.renderCard(item) }
+                    </Animated.View>
+                );
+            }
+            return this.props.renderCard(item);
+        });
     }
 
     cardAnimationStyle(){
@@ -49,21 +74,26 @@ export default class Deck extends Component {
         };
     }
 
-    renderCards(){
-        return this.props.data.map((item, index)=> {
-            if(index === 0){
-                return(
-                    <Animated.View
-                        key={ item.id }
-                        style={ [styles.container, this.cardAnimationStyle()] }
-                        {...this.state.panResponder.panHandlers}
-                    >
-                        { this.props.renderCard(item) }
-                    </Animated.View>
-                );
-            }
-            return this.props.renderCard(item);
-        });
+    resetPosition(){
+        Animated.timing(this.state.position, {
+            toValue: {x: 0, y: 0}
+        }).start();
+    }
+
+    swipeCard(direction){
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+            toValue: {x, y: 0},
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+
+    onSwipeComplete(direction){
+        const { onSwipeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.state.index];
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        this.state.position.setValue({ x: 0, y: 0});
+        this.setState({ index: this.state.index + 1});
     }
 
     render(){
