751c5b80deb931236ea70d7fd98e5e59a8c9e854
Pedro Silva <pedro.rls93@gmail.com>
2018-06-04 00:56:31 -0300

    import corrections on app/index

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Home from './Screens/Home';
+import { Home } from './Screens/Home';
+import { Login } from './Screens/Login';
 
 export default () => (
-    <Home />
+    <Login />
 );
