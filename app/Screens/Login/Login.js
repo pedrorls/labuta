1c9fddcaad9a21cda8857abd4022e9429130ab55
Pedro Silva <pedro.rls93@gmail.com>
2018-06-04 00:47:45 -0300

    keyboard usability improvements

--- app/Screens/Login/Login.js
+++ app/Screens/Login/Login.js
@@ -1,37 +1,50 @@
 import React, { Component } from 'react';
-import { KeyboardAvoidingView, View } from 'react-native';
+import { KeyboardAvoidingView, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { Input, Text, Button, SocialIcon } from 'react-native-elements';
-import firebase from '../../config/firebase';
+import { firebase, auth } from '../../config/firebase';
 import styles from './styles';
 
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = { email: '', pass: ''};
+    }
+
+    onLoginPress(){
+    }
+
     render(){
         return (
-            <View  style={ styles.container }>
-                    <Text h2>Login</Text>
-                    <Input
-                        placeholder='Email'
-                        leftIcon={{ name: 'email' }}
+            <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+                <View  style={ styles.container }>
+                        <Text h2>Login</Text>
+                        <Input
+                            placeholder='Email'
+                            leftIcon={{ name: 'email' }}
+                            onChangeText={ email => this.setState({email}) }
+                        />
+                        <Input
+                            placeholder='Password'
+                            leftIcon={{ name: 'lock' }}
+                            secureTextEntry
+                            onchangeText={ pass => this.setState({pass}) }
+                        />
+                    <Button
+                        title='Login'
+                        buttonStyle={ styles.loginButton }
+                        titleStyle={ styles.loginButtonText }
+                        onPress={this.onLoginPress()}
                     />
-                    <Input
-                        placeholder='Password'
-                        leftIcon={{ name: 'lock' }}
-                        secureTextEntry
+                    <Text h5> or </Text>
+                    <SocialIcon
+                        title='Sign in with Facebook'
+                        button
+                        type='facebook'
+                        style={{ width: 300, borderRadius: 10 }}
+                        disabled
                     />
-                <Button
-                    title='Login'
-                    buttonStyle={ styles.loginButton }
-                    titleStyle={ styles.loginButtonText }
-                />
-                <Text h5> or </Text>
-                <SocialIcon
-                    title='Sign in with Facebook'
-                    button
-                    type='facebook'
-                    style={{ width: 300, borderRadius: 10 }}
-                    disabled
-                />
-            </View>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
