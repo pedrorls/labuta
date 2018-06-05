41091700bd17a9a732d851e5abd3d4745527e442
Pedro Silva <pedro.rls93@gmail.com>
2018-06-05 20:19:35 -0300

    frontend for signup screen done

--- app/Screens/Signup/Signup.js
+++ app/Screens/Signup/Signup.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
-import { Input, Button, Text } from 'react-native-elements';
+import { View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Input, Button, Text, SocialIcon } from 'react-native-elements';
+import { Space } from '../../Components/Space';
 import styles from './styles';
 
 
 export default class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.state = {'email': '', 'password': '', 'passConfirmation': ''}
+    }
     render(){
         return(
-            <Text>SignUp</Text>
+            <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+                <View style={ styles.container }>
+                    <Text h3 style={styles.displayText}> Signup</Text>
+                    <Input
+                        placeholder={ 'Email' }
+                        leftIcon={{name: 'email'}}
+                        onChangeText= { email => this.setState({ email }) }
+                    />
+                    <Space/>
+                    <Input
+                        placeholder={ 'Password' }
+                        leftIcon={{name: 'lock'}}
+                        onChangeText= { pass => this.setState({ pass }) }
+                        secureTextEntry
+                    />
+                    <Space/>
+                    <Input
+                        placeholder={ 'Password Again' }
+                        leftIcon={{name: 'lock'}}
+                        onChangeText= { pass_confirmation => this.setState({ passConfirmation }) }
+                        secureTextEntry
+                    />
+                    <Button
+                        title={ 'Sign up' }
+                        buttonStyle={ styles.buttonStyle }
+                        titleStyle={ styles.buttonTextStyle }
+                        onPress={() => console.log('Sign me up!')}
+                    />
+                    <Text h5>or</Text>
+                    <SocialIcon
+                        title={ 'Sign up with Facebook' }
+                        button
+                        type='facebook'
+                        style={{ width: 300, borderRadius: 10 }}
+                        onPress={() => console.log('Sign me up with my facebook')}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
--- app/Screens/Signup/styles.js
+++ app/Screens/Signup/styles.js
@@ -2,6 +2,23 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonStyle: {
+        width: 300,
+        height: 50,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        borderWidth: 1.5,
+        borderColor: '#2B60DE',
+        marginTop: 20,
+    },
+    buttonTextStyle: {
+        color: '#2B60DE'
+    },
+    displayText: {
+        marginVertical: 50
     }
 });
